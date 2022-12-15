c781b89c87e0b10395fd917d46902ab314268558
Gordon D <gordon.draine97@yahoo.com>
2022-12-14 23:10:12 -0500

    adding carousel from compenent list

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+import Navbar from './Navbar.js';
+// import components. 
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   < Navbar />
+   
+  )
+}
+
+// include components in fragment.
+
+export default App
